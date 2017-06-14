4eaed74f1eee7bf051f5d5d57f34b4935b7b02be
Sun,Steven <ssun21@ITS.jnj.com>
2017-06-14 12:53:33 -0400

    Initial commit

---
+++ 3_src/model.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+from sklearn.externals import joblib
+
+best_perf_model = joblib.load()
+
+
+# Set of prediction functions that expect
+# a singular |patient_data| in the form of Index or pd.df
+# and returns boolean:
+# True for LOS >= 4
+# False for LOS <= 3
+
+# predict using full feature model
+def full_predict(patient_data):
+    los = True;
+    return los;
+
+# predict using sparse model
+def sparse_predict(patient_data):
+    los = True;
+    return los;
+
+# predict using hybrid model
+def hybrid_predict(patient_data):
+    los = True;
+    return los;
+
+# predict using neural net model
+def nn_predict(patient_data):
+    los = True;
+    return los;
+
+# trains selected model on given data
+# manual selection of features optional
+def train(data,model_type,selection):
+    data = df_transform(data)
+
+    if(selection):
+        data = feature_reduce(data,selection)
+    if(model_type = "sparse"):
+        generate_sparse_model(data)
+    else if(model_type = "hybrid"):
+        generate_hybrid_model(data)
+    else if(model_type = "nn"):
+        generate_nn(data)
+    else:
+        generate_full_model(data)
+
+# generates selected model based on given model_type request
+# Note to reader: These functions are currently being tested in notebooks
+# Implementation is a simple copy and paste
+def generate_full_model(data):
+    return pickle
+def generate_sparse_model(data):
+    return pickle
+def generate_hybrid_model(data):
+    return pickle
+def generate_nn(data):
+    return pickle
+
+# Reduces a dataset using set of feature reduction techniques
+# |data| is pd.df form of raw data
+# |selection| is Index of additional manual selections
+# |reduced| is pd.df of data with with reduced featureset
+def feature_reduce(data,selection):
+
+    reduced = []
+    # feat_ranking is an array of arrays
+    # in key-value form
+    # ['Technique',['feature',performance_score]]
+    # example feat_ranking = ['PCA',['Age', 0.7]]
+    feat_ranking = [['PCA',0],[]]
+
+    technique_usage = feature_selection_technique_eval(feat_ranking)
+
+    # Thomas is working on
+    return reduced
+
+# Expects feat_ranking as described in feature_reduce
+# returns array of best performing feature reduction techniques
+# Ex. ['PCA','LinSVM']
+def feature_selection_technique_eval(feat_ranking):
+    return []
+
+# Expects dataset in pd.df form
+# Expects perf_metrics in Index
+# ['Type 1': Cost, 'Type 2': Cost]
+# Runs full,sparse,hybrid,and neural net models
+# Evaluates model performance against perf_metrics supplied
+# returns best model stored as .pkl
+# Warning: calling this function takes a long time
+def create_best_model(data,perf_metrics):
+
+# Identifies dataset format
+# Coerces dataset into usable pd.df
+def df_transform(data):
--- 3_src/server.py
+++ 3_src/server.py
@@ -1,17 +1,151 @@
+import model
 from flask import Flask, jsonify, request, render_template
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+# Router for prediction using model developed off full featureset
+# Pros: Better at generalizing
+# Cons: Subject to high bias internal to data
+@app.route('/predict_full', methods=['POST'])
+def full_model_handler():
     if request.method == 'POST':
         # Print JSON data
         input_data = request.get_json(force=True)
         # Read the test key from received JSON
         print input_data['test']
+        # JSON to Index or pd.df
+        new_patient = model.df_transform(input_data)
         # Run model on input data here
-    # Send result back
-    data = { 'days_of_stay': 8 }
-    return jsonify(data)
+        prediction = model.full_predict(new_patient)
+        data = { 'LOS > 3': prediction}
+        return jsonify(data)
 
+        except Exception, e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print 'train first'
+        return 'no model here'
+
+# Router for prediction using model developed off sparse featureset
+# Pros: Incorporates additional features with high skew variance
+# Cons: Subject to overestimating impact of low-occurence features
+@app.route('/predict_sparse', methods=['POST'])
+def sparse_model_handler():
+    if request.method == 'POST':
+        # Print JSON data
+        input_data = request.get_json(force=True)
+        # Read the test key from received JSON
+        print input_data['test']
+        # JSON to Index or pd.df
+        new_patient = model.df_transform(input_data)
+        # Run model on input data here
+        prediction = model.sparse_predict(new_patient)
+        data = { 'LOS > 3': prediction}
+        return jsonify(data)
+
+        except Exception, e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print 'train first'
+        return 'no model here'
+
+# Router for prediction using hybrid model
+# Pros: Tries to work in trade-off between sparse and full model predictions
+# Cons: Trying to do both may lead to being good at neither
+@app.route('/predict_hybrid', methods=['POST'])
+def full_model_handler():
+    if request.method == 'POST':
+        # Print JSON data
+        input_data = request.get_json(force=True)
+        # Read the test key from received JSON
+        print input_data['test']
+        # JSON to Index or pd.df
+        new_patient = model.df_transform(input_data)
+        # Run model on input data here
+        prediction = model.hybrid_predict(new_patient)
+        data = { 'LOS > 3': prediction}
+        return jsonify(data)
+
+        except Exception, e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print 'train first'
+        return 'no model here'
+
+# Router for prediction using neural network
+# Pros: Will likely outperform other models at high data throughput
+# Cons: Unlikely that sufficient high throughput will occur
+@app.route('/predict_nn', methods=['POST'])
+def full_model_handler():
+    if request.method == 'POST':
+        # Print JSON data
+        input_data = request.get_json(force=True)
+        # Read the test key from received JSON
+        print input_data['test']
+        # JSON to Index or pd.df
+        new_patient = model.df_transform(input_data)
+        # Run model on input data here
+        prediction = model.predict_nn(new_patient)
+        data = { 'LOS > 3': prediction}
+        return jsonify(data)
+
+        except Exception, e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print 'train first'
+        return 'no model here'
+
+# Router for command to retrain a given model
+# Expects JSON with:
+# { model_to_retrain: "", data:"" }
+# retrains selected model with provided dataset
+@app.route('/retrain', methods=['POST'])
+def retrain(model,data):
+
+    model.df_transform(data)
+
+    if(model == "full"):
+        model.generate_full_model(data)
+
+    else if(model == "sparse"):
+        model.generate_sparse_model(data)
+
+    else if(model == "hybrid"):
+        model.generate_hybrid_model(data)
+
+    else if(model == "nn"):
+        model.generate_nn(data)
+
+    else:
+        print 'please make sure selection was a serviceable model'
+        return 'error in model retraining'
+
+@app.route('/create_best_model', methods=['POST'])
+def best_model(data,perf_metrics):
+    data = model.df_transform(data)
+    model.create_best_model(data,perf_metrics)
+
+# Runner function
 if __name__ == '__main__':
-    app.run()
+    try:
+        port = int(sys.argv[1])
+    except Exception, e:
+        port = 80
+
+    try:
+        clf = joblib.load(model_file_name)
+        print 'model loaded'
+        model_columns = joblib.load(model_columns_file_name)
+        print 'model columns loaded'
+
+    except Exception, e:
+        print 'No model here'
+        print 'Train first'
+        print str(e)
+        clf = None
+
+    app.run(host='0.0.0.0', port=port, debug=True)
